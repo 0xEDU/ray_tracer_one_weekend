069c27a59aee36de32b4b097f69dac2c9607b2cb
etachott <etachott@student.42sp.org.br>
2023-04-15 13:23:11 -0300

    prepared main for final render

--- sources/main.cc
+++ sources/main.cc
@@ -71,11 +71,11 @@ color ray_color(const ray &r, const hittable& world, int depth) {
 
 int	main(void) {
 	// Image
-	const auto aspect_ratio = 3.0 / 2.0;
-	const int image_width = 200;
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 1920;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 25;
-	const int max_depth = 10;
+	const int samples_per_pixel = 500;
+	const int max_depth = 100;
 
 	// World
 	auto world = random_scene();
